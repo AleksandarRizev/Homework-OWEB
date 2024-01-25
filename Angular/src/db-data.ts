0e77560f7217fd24baa5d2fae165e686538116f6
AleksandarRizev <arizev23@gmail.com>
2024-01-25 21:58:34 +0100

    commit

---
+++ Angular/src/db-data.ts
@@ -0,0 +1,47 @@
+export const DRIVERS: any = [
+
+    //undefined
+
+    {
+        id: 1,
+        name: "Lewis Hamilton",
+        iconUrl: 'https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/hamilton.jpg.img.1920.medium.jpg/1677069594164.jpg',
+        longDescription: "Greatest of all time",
+        category: 'Veteran',
+        points: 234
+    },
+    {
+        id: 2,
+        name: "George Russell",
+        iconUrl: 'https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/russell.jpg.img.1920.medium.jpg/1701269180717.jpg',
+        longDescription: "Champion in a few years",
+        category: 'Upcoming',
+        points: 175
+    },
+    {
+        id: 3,
+        name: "Carlos Sainz",
+        iconUrl: 'https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/sainz.jpg.img.1920.medium.jpg/1677069189406.jpg',
+        longDescription: "Oldest to score points",
+        category: 'Veteran',
+        points: 200
+    },
+    {
+        id: 4,
+        name: "Logan Sargeant",
+        iconUrl: 'https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/sargeant.jpg.img.1920.medium.jpg/1701272060171.jpg',
+        longDescription: "Bad first season",
+        category: 'Rookie',
+        points: 1
+    },
+    {
+        id: 5,
+        name: "Oscar Piastri",
+        iconUrl: 'https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/piastri.jpg.img.1920.medium.jpg/1676983075734.jpg',
+        longDescription: "Best rookie season since Hamilton",
+        category: 'Rookie',
+        points: 97
+    }
+
+
+];
